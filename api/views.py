cf9e36cb036eccb71dda57eabf6aecdf26a9127c
Adham Salama <adhamsalama@zohomail.com>
2021-07-14 14:41:53 +0200

    Use Generics

--- api/views.py
+++ api/views.py
@@ -1,42 +1,21 @@
 from .models import Note
 from .serializers import NoteSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
 # Create your views here.
 
-class NotesList(generics.GenericAPIView,
-                mixins.ListModelMixin,
-                mixins.CreateModelMixin):
+class NotesList(generics.ListCreateAPIView):
     """
     List all notes.
     """
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
 
-
-class NoteDetail(generics.GenericAPIView,
-                mixins.RetrieveModelMixin,
-                mixins.UpdateModelMixin,
-                mixins.DestroyModelMixin):
+class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a note."
     """
     
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
