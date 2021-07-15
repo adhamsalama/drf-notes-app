967ad6905dcbaf232b2713a8becbf5f2cc576216
Adham Salama <adhamsalama@zohomail.com>
2021-07-15 17:58:56 +0200

    Add title field to Note model

--- api/models.py
+++ api/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from accounts.models import User
 
 # Create your models here.
 
+class User(AbstractUser):
+    pass
+
+
 class Note(models.Model):
+    title = models.CharField(max_length=64)
     content = models.TextField(blank=True)
-    #user = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
     date = models.DateTimeField(auto_now_add=True)
+    
