77c9edfda137da561e0a6024e4ffe26261e96483
Adham Salama <adhamsalama@zohomail.com>
2021-07-15 18:19:27 +0200

    Create an endpoint for the root of the API

--- api/views.py
+++ api/views.py
@@ -3,12 +3,12 @@ from .serializers import NoteSerializer, UserSerializer
 from rest_framework import generics, permissions
 from .permissions import IsOwnerOrReadOnly, IsOwner
 from rest_framework.decorators import api_view
-from rest_framework.respone import Response
+from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
 # Create your views here.
 
-@apie_view(['GET'])
+@api_view(['GET'])
 def api_root(request, format=None):
     return Response({
         'users': reverse('user-list', request=request, format=format),
