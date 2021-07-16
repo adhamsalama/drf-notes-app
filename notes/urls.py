f4a3498c97521f519052ba236300475d51c8a77f
Adham Salama <adhamsalama@zohomail.com>
2021-07-16 19:44:12 +0200

    Change urls for frontend

--- notes/urls.py
+++ notes/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
-    path('frontend', include('frontend.urls')),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api/', include('api.urls')),
+    path('', include('frontend.urls')),
+    path('api-auth/', include('rest_framework.urls'), name="auth")
 ]
