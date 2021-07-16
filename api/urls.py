fc7abc0ee82efd3f3a205e78e1147197e6cdd5dc
Adham Salama <adhamsalama@zohomail.com>
2021-07-16 07:50:50 +0200

    Add names for urls

--- api/urls.py
+++ api/urls.py
@@ -4,10 +4,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
-    path('notes', views.NoteList.as_view()),
+    path('notes', views.NoteList.as_view(), name='note-list'),
     path('notes/<int:pk>', views.NoteDetail.as_view()),
-    path('users', views.UserList.as_view()),
-    path('users/<int:pk>', views.UserDetail.as_view())
+    path('users', views.UserList.as_view(), name='user-list'),
+    path('users/<int:pk>', views.UserDetail.as_view()),
+    path('', views.api_root)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
